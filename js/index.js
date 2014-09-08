324437cb8ffa90cb51bdb7113e6b6f77bd58faf8
koturn <jeak.koutan.apple@gmail.com>
2014-09-09 03:59:36 +0900

    Use history API

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 'use strict';
 
-function readContent(filename) {
+function readContent(filename, urlSuffix, isAddHistory) {
+  if (isAddHistory == undefined) {
+    isAddHistory = true;
+  }
   var httpReq = new XMLHttpRequest();
   httpReq.open('GET', filename, true);
   httpReq.onreadystatechange = function() {
     if (httpReq.readyState == 4) {
       document.getElementById('main-area').innerHTML = httpReq.responseText;
+      if (isAddHistory) {
+        var state = {
+          filename: filename,
+          urlSuffix: urlSuffix
+        };
+        history.pushState(state, null, urlSuffix);
+      }
     }
   }
   httpReq.send(null);
 }
+
+window.addEventListener('popstate', function(e) {
+  readContent(history.state.filename, history.state.urlSuffix, false);
+}, false);
